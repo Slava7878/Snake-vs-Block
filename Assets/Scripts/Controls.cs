94ed160a0e82ec97fc09d971987a5e5dc9c7799c
Slava7878 <105912871+Slava7878@users.noreply.github.com>
2022-07-27 22:52:42 +0500

    Added new controls

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -4,31 +4,41 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
-    public Rigidbody Player;    
+    //public Rigidbody Player;
+    //public float Sensitivity;
+    //Vector3 initialPosition;
+    //private float _distance;
 
-    public float Sensitivity;
-
-    Vector3 initialPosition;
-    
-    private float _distance;
+    public Transform PlayerObj;
+    public float Speed;
 
     void Update()
     {
         //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0))
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    initialPosition = transform.position;
+        //    Vector3 rayPoint = ray.GetPoint(0);
+        //    _distance = Vector3.Distance(transform.position, rayPoint);
+        //}
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    Vector3 rayPoint = ray.GetPoint(_distance * Sensitivity);
+        //    Player.MovePosition(initialPosition + new Vector3(rayPoint.x, 0, 0));
+        //}
+
+        if (Input.GetKey(KeyCode.A))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            initialPosition = transform.position;
-            Vector3 rayPoint = ray.GetPoint(0);
-            _distance = Vector3.Distance(transform.position, rayPoint);
+            PlayerObj.position += -PlayerObj.right * (Time.deltaTime * Speed);
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetKey(KeyCode.D))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 rayPoint = ray.GetPoint(_distance * Sensitivity);
-            Player.MovePosition(initialPosition + new Vector3(rayPoint.x, 0, 0));
+            PlayerObj.position += PlayerObj.right * (Time.deltaTime * Speed);
         }
     }    
 }
